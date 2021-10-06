4b988981e9b7ab8a9c38446f60268d11bafc10eb
guoziren <234917515@qq.com>
2021-10-06 14:04:57 +0800

    项目初始化

---
+++ mall-coupon/src/main/java/com/lycguo/mall/coupon/CouponApplication.java
@@ -0,0 +1,11 @@
+package com.lycguo.mall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CouponApplication.class);
+    }
+}
