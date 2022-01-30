134078a82b375398f8765e62126014ed12d966db
guoziren <234917515@qq.com>
2022-01-30 16:23:59 +0800

    product加入到注册中心

--- mall-product/src/test/java/com/lycguo/mall/product/ProductApplicationTest.java
+++ mall-product/src/test/java/com/lycguo/mall/product/ProductApplicationTest.java
@@ -2,18 +2,18 @@ package com.lycguo.mall.product;
 
 import com.lycguo.mall.product.entity.BrandEntity;
 import com.lycguo.mall.product.service.BrandService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
-class ProductApplicationTest {
+public class ProductApplicationTest {
     @Autowired
     BrandService brandService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
         BrandEntity brandEntity = new BrandEntity();
         brandEntity.setDescript("hello");
         brandEntity.setName("苹果");
