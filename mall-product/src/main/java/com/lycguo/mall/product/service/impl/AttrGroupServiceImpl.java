780b9e0b4eb82d724aaad33dfab04a4634268f29
guoziren <234917515@qq.com>
2022-02-05 16:04:46 +0800

    属性分组增加根据分类id查询的方法

--- mall-product/src/main/java/com/lycguo/mall/product/service/impl/AttrGroupServiceImpl.java
+++ mall-product/src/main/java/com/lycguo/mall/product/service/impl/AttrGroupServiceImpl.java
@@ -1,16 +1,17 @@
 package com.lycguo.mall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lycguo.common.utils.PageUtils;
 import com.lycguo.common.utils.Query;
-
 import com.lycguo.mall.product.dao.AttrGroupDao;
 import com.lycguo.mall.product.entity.AttrGroupEntity;
 import com.lycguo.mall.product.service.AttrGroupService;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
 
 
 @Service("attrGroupService")
@@ -24,6 +25,28 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, Long catelogId) {
+        // select * from pms_attr_group where catelog_id=? and (attr_group_id=key or attr_group_name like  %key%)
+        String key = (String) params.get("key");
+        QueryWrapper<AttrGroupEntity> wrapper = new QueryWrapper<AttrGroupEntity>();
+        if (!StringUtils.isEmpty(key)) {
+            wrapper.and((obj) -> {
+                obj.eq("attr_group_id", key).or().like("attr_group_name", key);
+            });
+        }
+        if (catelogId == 0) {
+            IPage<AttrGroupEntity> page = this.page(new Query<AttrGroupEntity>().getPage(params), wrapper);
+            return new PageUtils(page);
+        } else {
+
+            wrapper.eq("catelog_id", catelogId);
+            IPage<AttrGroupEntity> page = this.page(new Query<AttrGroupEntity>().getPage(params), wrapper);
+            return new PageUtils(page);
+
+        }
     }
 
 }
